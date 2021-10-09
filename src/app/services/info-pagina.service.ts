a1ca65cfe50e65ec7063ab55c51295020bcd145c
Daniel Méndez Yáñez <dmendezy87@gmail.com>
2021-10-09 15:19:55 -0500

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,14 +10,29 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada: boolean = false;
 
+  equipo: any = [];
+
   constructor( private http: HttpClient ) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
              .subscribe( (resp: InfoPagina) => {
                this.cargada = true;
                this.info = resp;
              });
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://curso-angular-html-9ee9a-default-rtdb.firebaseio.com/equipo.json')
+             .subscribe( resp => {
+               this.equipo = resp;
+               console.log(this.equipo);
+             });
   }
 
 }
