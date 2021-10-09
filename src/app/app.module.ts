01693fa074baf9771a7885a6d6ca31387489896c
Daniel Méndez Yáñez <dmendezy87@gmail.com>
2021-10-09 13:43:16 -0500

    Fin sección 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ItemComponent } from './pages/item/item.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    AboutComponent,
     FooterComponent,
+    HeaderComponent,
+    ItemComponent,
     PortafolioComponent
   ],
   imports: [
